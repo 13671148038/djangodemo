2295333186c5f1b5eb3b00d920b9582df8799b86
13671148038 <8672252zhu>
2018-12-20 16:54:33 +0800

    二测

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import Question, Choice
 
+
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+    list_display = ('pub_date', 'question_text')
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('baseInfo', {'fields': ['choice_text', 'question_id']}),
+        ('time', {'fields': ['votes']})
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
